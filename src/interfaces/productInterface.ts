b24f27afd2ec9d7f32da8ce164eb79928a73de2d
Orestarxx <oresto322@gmail.com>
2024-10-07 22:28:04 +0300

    did some interfaces

---
+++ src/interfaces/productInterface.ts
@@ -0,0 +1,42 @@
+interface IProduct {
+
+    id: number;
+    title: string;
+    description: string;
+    category: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    tags: string[];
+    brand: string;
+    sku: string;
+    weight:number;
+    dimensions: {
+        width:number;
+        height: number;
+        depth: number;
+    },
+    warrantyInformation: number;
+    shippingInformation: string;
+    availabilityStatus: string;
+    reviews: IReviews[];
+    returnPolicy: string;
+    minimumOrderQuantity: number;
+    meta: {
+        createdAt: string;
+        updatedAt: string;
+        barcode: string;
+        qrCode: string;
+    },
+    images:string[]
+    thumbnail: string;
+}
+interface IReviews {
+    comment:string;
+    date: string;
+    rating:number;
+    reviewerEmail:string;
+    reviewerName:string;
+}
+export type {IProduct}
