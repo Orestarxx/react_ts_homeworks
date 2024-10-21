d81e55819f4ff290f0821fa51d6d1a386ee53baa
Orestarxx <oresto322@gmail.com>
2024-10-21 15:50:24 +0300

    done homework

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -5,6 +5,7 @@ import './navigationStyle.css';
 const Navigation = () => {
     return (
         <div id={'navigation'}>
+            <NavLink to={'/'} className={'navigationLink'}>Home</NavLink>
             <NavLink to={'posts'} className={'navigationLink'}>Posts</NavLink>
             <NavLink to={'users'} className={'navigationLink'}>users</NavLink>
             <NavLink to={'products'} className={'navigationLink'}>products</NavLink>
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,11 +3,13 @@ import MainLayOut from "../mainLayOut/MainLayOut";
 import Posts from "../components/postsComponents/posts/Posts";
 import Users from "../components/usersComponents/users/Users";
 import Products from "../components/productsComponents/products/Products";
+import HomePage from "../components/homePage/HomePage";
 
 export const routes = createBrowserRouter([
     {path:'/',element:<MainLayOut/>,children:[
         {path:'posts', element:<Posts/>},
             {path:'users',element:<Users/>},
-            {path:'products', element:<Products/>}
+            {path:'products', element:<Products/>},
+            {index:true,element:<HomePage/>}
         ]}
 ])
